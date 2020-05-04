48666e68ad8e26a6f0ebc4a8cd3dfae120664dd9
darkchiii <dariawnk@gmail.com>
2020-05-04 23:01:54 +0200

    zmienilam fonty na ten z projektu, dodalam ikony i zdjecia

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import "react-native-gesture-handler";
 import React, { useState } from "react";
 import * as Font from "expo-font";
-import Home from "./screens/Home";
+import HomeScreen from "./screens/Home";
 import { AppLoading } from "expo";
 import Navigator from "./routes/homeStack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 
+
+{/*
 const getFonts = () =>
   Font.loadAsync({
     "lato-regular": require("./assets/fonts/Lato-Regular.ttf"),
     "lato-bold": require("./assets/fonts/Lato-Bold.ttf"),
     "lato-light": require("./assets/fonts/Lato-Light.ttf"),
     "lato-black": require("./assets/fonts/Lato-Black.ttf"),
+  });*/}
+
+  const getFonts = () =>
+  Font.loadAsync({
+    "inter-bold": require("./assets/fonts/Inter-Bold.ttf"),
+    "inter-regular": require("./assets/fonts/Inter-Regular.ttf"),
+    "inter-semiBold": require("./assets/fonts/Inter-SemiBold.ttf"),
+    "inter-medium": require("./assets/fonts/Inter-Medium.ttf"),
   });
 
 export default function App() {
@@ -20,6 +32,7 @@ export default function App() {
     return <Navigator />;
   } else {
     return (
+
       <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
     );
   }
---
+++ screens/Marketplace.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text} from "react-native" ;
+
+export default function Marketplace() {
+    return (
+        <Text>Które rośliny chcesz hodować?</Text>
+
+
+    );
+}
+
+const styles = StyleSheet.create ({
+
+
+    
+})
